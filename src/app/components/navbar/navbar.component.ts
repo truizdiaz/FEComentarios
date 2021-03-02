f10b253fe6eb746c1e199caec2204b3b51546c7b
Tomas Ruiz Diaz <tomiruizdiaz@gmail.com>
2021-03-02 15:32:26 -0300

    NavbarComponent]

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png';
 
   constructor() { }
 
