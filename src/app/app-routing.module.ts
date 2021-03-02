d3d9d416e06abc8e1d25795d3c4874b6f462837a
Tomas Ruiz Diaz <tomiruizdiaz@gmail.com>
2021-03-02 16:24:53 -0300

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgregarEditarComentarioComponent } from './components/agregar-editar-comentario/agregar-editar-comentario.component';
+import { ListComentariosComponent } from './components/list-comentarios/list-comentarios.component';
+import { VerComentarioComponent } from './components/ver-comentario/ver-comentario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListComentariosComponent },
+  { path: 'agregar', component: AgregarEditarComentarioComponent },
+  { path: 'editar/:id', component: AgregarEditarComentarioComponent },
+  { path: 'ver/:id', component: VerComentarioComponent },
+  { path: '**', redirectTo: '/', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
